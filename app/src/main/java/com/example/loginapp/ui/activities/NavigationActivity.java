ea35c419ef08c12a391b123b830e34fcd80edeb6
Mohamed <mmdabd6@gmail.com>
2019-05-20 21:37:16 +0200

    add Nav

---
+++ app/src/main/java/com/example/loginapp/ui/activities/NavigationActivity.java
@@ -0,0 +1,15 @@
+package com.example.loginapp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.loginapp.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
